288144dfb4dc58d47799da4f2f15ad475914d756
anhtran <trananh30044003@gmail.com>
2023-04-01 18:45:30 +0700

    Update hot feature content of Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import ProductsFeatured from '../components/products-featured';
 import Footer from '../components/footer';
 import Subscribe from '../components/subscribe';
 import {BlogInHome} from "../components/Home/BlogInHome";
+import {HotFeatures} from "../components/Home/HotFeatures";
 
 const IndexPage = () => {
   return (
@@ -12,48 +13,7 @@ const IndexPage = () => {
 
       <BlogInHome/>
 
-      <section className="section">
-        <div className="container">
-          <header className="section__intro">
-            <h4>Why should you choose us?</h4>
-          </header>
-
-          <ul className="shop-data-items">
-            <li>
-              <i className="icon-shipping"></i>
-              <div className="data-item__content">
-                <h4>Free Shipping</h4>
-                <p>All purchases over $199 are eligible for free shipping via USPS First Class Mail.</p>
-              </div>
-            </li>
-
-            <li>
-              <i className="icon-payment"></i>
-              <div className="data-item__content">
-                <h4>Easy Payments</h4>
-                <p>All payments are processed instantly over a secure payment protocol.</p>
-              </div>
-            </li>
-
-            <li>
-              <i className="icon-cash"></i>
-              <div className="data-item__content">
-                <h4>Money-Back Guarantee</h4>
-                <p>If an item arrived damaged or you've changed your mind, you can send it
-                  back for a full refund.</p>
-              </div>
-            </li>
-
-            <li>
-              <i className="icon-materials"></i>
-              <div className="data-item__content">
-                <h4>Finest Quality</h4>
-                <p>Designed to last, each of our products has been crafted with the finest materials.</p>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <HotFeatures/>
 
       <ProductsFeatured/>
       <Subscribe/>
