c16be67699eb1be5a7dedb5246c61c1e0025ee3f
anhtran <trananh30044003@gmail.com>
2023-04-01 18:54:46 +0700

    Update contact content of Home Page

--- components/subscribe/index.tsx
+++ components/subscribe/index.tsx
@@ -3,11 +3,12 @@ const Subscribe = () => {
     <section className="subscribe">
       <div className="container">
         <div style={{backgroundImage: 'url(/images/subscribe.jpg)'}} className="subscribe__content">
-          <h4>Subscribe to our newsletter and receive exclusive offers every week</h4>
+          <h4>Đăng ký nhận bản tin của chúng tôi và nhận các ưu đãi độc quyền hàng tuần.</h4>
+          {/*<h4>Subscribe to our newsletter and receive exclusive offers every week</h4>*/}
 
           <form className="subscribe__form">
             <input type="email" placeholder="Email address" />
-            <button type="submit" className="btn btn--rounded btn--yellow">Subscribe</button>
+            <button type="submit" className="btn btn--rounded btn--yellow">Đăng ký ngay</button>
           </form>
         </div>
       </div>
