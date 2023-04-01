b446c12090a30bf4a766a59a5f2fae0cd1b8c234
anhtran <trananh30044003@gmail.com>
2023-04-01 20:42:50 +0700

    Update content product detail page

--- components/product-single/description/index.tsx
+++ components/product-single/description/index.tsx
@@ -11,8 +11,8 @@ const Description = ({ show }: ProductDescriptionType) => {
     <section style={style} className="product-single__description">
       <div className="product-description-block">
         <i className="icon-cart"></i>
-        <h4>Details and product description</h4>
-        <p>White Summer Vibes T-shirt in the uiKit line with a colorful print. <br></br>Made of jersey cotton. T-shirt fits perfectly with jeans, pants or shorts.</p>
+        <h4>Mô tả chi tiết về sản phẩm</h4>
+        <p>Áo phông trắng Summer Vibes thuộc dòng uiKit với hình in đầy màu sắc. <br></br>Được làm bằng bông jersey. Áo phông hoàn toàn phù hợp với quần jean, quần dài hoặc quần short.</p>
       </div>
       <div className="product-description-block">
         <i className="icon-cart"></i>
