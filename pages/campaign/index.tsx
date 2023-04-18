9c51385ae033357005337d1e1ee1def2c835bb1d
Anh Tran <trananh30044003@gmail.com>
2023-04-18 20:19:20 +0700

    add campaign pages

---
+++ pages/campaign/index.tsx
@@ -0,0 +1,118 @@
+import React, {useEffect, useState} from "react";
+import Layout from '../../layouts/Main';
+import Image from "next/image";
+import ProductsFilter from "../../components/products-filter";
+import ProductsContent from "../../components/products-content";
+import Footer from "../../components/footer";
+import {getListProduct} from "../../lib/API";
+import {InputProduct, Product, Campaign} from "../../types";
+import {dataInputProducts} from "../products";
+import {getListCampaign} from "../../lib/Campaign/API";
+import Link from "next/link";
+export function GetDefaultCampaign() {
+    const data = {
+        id: 0,
+        name: '',
+        thumb: '',
+        sku: '',
+        start_day: '',
+        end_day: '',
+        status: 0,
+        campaign_description: ""
+    }
+    return data
+}
+export default function CampaignPage(){
+    const [products, setProducts] = useState<Product[]>([])
+    const [filterProduct, setFilterProduct] = useState<InputProduct>(dataInputProducts());
+    const [isOpenAlert, setIsOPenAlert] = useState(false);
+    const [textErrorAPI, setTextErrorAPI] = useState("");
+    const [campaignSelected, setCampaignSelected] = useState<Campaign>(GetDefaultCampaign())
+    useEffect(() =>{
+        async function fetchCampaignData() {
+            try {
+                const res = await getListCampaign()
+                const status = res.code;
+                if (status === 200) {
+                    for(let i = 0; i < res.data.length; i++){
+                        setCampaignSelected(res.data[i]);
+                    }
+                } else {
+                    console.log('error');
+                }
+            } catch (e) {
+                console.log('error');
+            }
+        }
+
+        fetchCampaignData().then();
+    }, [])
+    useEffect(() =>{
+        async function fetchProductData() {
+            try {
+                const res = await getListProduct(filterProduct)
+                const status = res.code;
+                if (status === 200) {
+                    setProducts(res.data);
+                } else {
+                    console.log('error');
+                    setTextErrorAPI("Không thể tải sản phẩm!")
+                    setIsOPenAlert(true);
+                }
+            } catch (e) {
+                console.log('error');
+            }
+        }
+        // console.log("statusUpdate", statusUpdate);
+        fetchProductData().then();
+        console.log("filterProduct", filterProduct);
+    }, [filterProduct])
+    return<>
+        <Layout>
+            {/*<div className="header-campaign">*/}
+            {/*    <Image src="" alt="" layout="fill" objectFit="contain"*/}
+            {/*           objectPosition="center" />*/}
+            {/*    <h1></h1>*/}
+            {/*</div>*/}
+            {/*<div className="page-intro__slide" style={{ backgroundImage: `url(${campaignSelected.thumb})`, backgroundRepeat: "no-repeat",backgroundSize: "auto", width:"100%", height: '500px' }}>*/}
+            {/*    <div className="container">*/}
+            {/*        <div className="page-intro__slide__content">*/}
+            {/*            <h2>{campaignSelected.name}</h2>*/}
+            {/*            /!*<Link href={'/campaign/?sku=' + campaignSelected.sku} legacyBehavior>*!/*/}
+            {/*            /!*    <a className="btn-shop"><i className="icon-right"></i>Mua ngay</a>*!/*/}
+            {/*            /!*</Link>*!/*/}
+
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            <div className="banner-campaign container">
+                <div>
+                <Image src={campaignSelected.thumb} alt=""
+                       width={1200}
+                       height={500}
+                       // layout="fill"
+                       // objectFit="contain"
+                       // objectPosition="center"
+                />
+                </div>
+
+            </div>
+            {/*<div className="container text-campaign">*/}
+            {/*    <h1>{campaignSelected.name}</h1>*/}
+            {/*    <p>{campaignSelected.campaign_description}</p>*/}
+            {/*</div>*/}
+
+            <section className="products-page" style={{marginTop: "20px"}}>
+
+                <div className="container">
+                    <ProductsFilter filterProduct={filterProduct} setFilterProduct={setFilterProduct}/>
+                    <ProductsContent product={products} filterProduct={filterProduct} setFilterProduct={setFilterProduct}/>
+                </div>
+            </section>
+            <div>
+                <Footer/>
+            </div>
+
+        </Layout>
+    </>
+}
